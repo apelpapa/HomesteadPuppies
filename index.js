03dc9a0d722bf159d14ba7091110d160cb0d4670
apelpapa <apelpapa@gmail.com>
2025-02-26 01:39:45 -0700

    added env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import env from "dotenv"
 
 const app = new express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+env.config();
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(`${__dirname}/public`));
