70ef6a0e2591cd28cb581720fb1880faee7c6e65
apelpapa <apelpapa@gmail.com>
2025-02-26 19:27:51 -0700

    Added Cards and Database

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 import express from "express"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import pg from "pg"
 import env from "dotenv"
 
-const app = new express();
-const port = 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
 env.config();
+const app = new express();
+const port = process.env.SERVER_PORT;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(`${__dirname}/public`));
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+});
+
+db.connect()
 
 app.get("/", (req,res) => {
     res.render("index.ejs")
 })
 
-app.get("/availablePuppies", (req,res)=>{
-    res.render("availablePuppies.ejs")
+app.get("/availablePuppies", async (req,res)=>{
+    const result = await db.query("SELECT * FROM puppies");
+    const puppies = result.rows;
+    res.render("availablePuppies.ejs", {puppies: puppies})
 })
 
 app.get("/parents", (req,res)=>{
