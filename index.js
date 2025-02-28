1a774d334dc59941dde2644df275aeeb4227d379
apelpapa <apelpapa@gmail.com>
2025-02-27 20:21:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
 });
 
 db.connect();
